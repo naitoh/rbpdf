0511d96b640c40ee03e500c88416f9c51a30c8ca
NAITOH Jun <naitoh@gmail.com>
2018-08-15 21:52:49 +0900

    write_html_cell test refactoring.

--- test/rbpdf_htmlcell_test.rb
+++ test/rbpdf_htmlcell_test.rb
@@ -1,68 +1,39 @@
-# Copyright (c) 2011-2017 NAITOH Jun
+# Copyright (c) 2011-2018 NAITOH Jun
 # Released under the MIT license
 # http://www.opensource.org/licenses/MIT
 
 require 'test_helper'
 
 class RbpdfTest < Test::Unit::TestCase
-  test "write_html_cell Basic test" do
+  htmls = {
+    'Basic'                => {:html => '<p>foo</p>',
+                               :border => 0,      :pno => 1, :y => 17.3, :no => 1},
+    'Page Break no border' => {:html => '<p>foo</p>', :margin => 30,
+                               :border => 0,      :pno => 2, :y => 40.0, :no => 2},
+    'Page Break border'    => {:html => '<p>foo</p>', :margin => 30,
+                               :border => 'LRBT', :pno => 2, :y => 40.0, :no => 2},
+  }
+
+  data(htmls)
+  test "write_html_cell test" do |data|
     pdf = RBPDF.new
     pdf.add_page()
 
-    htmlcontent = '<p>foo</p>'
-    pdf.write_html_cell(0, 5, 10, '', htmlcontent, 0, 1)
+    if data[:margin]
+      pdf.set_top_margin(data[:margin])
+      h = pdf.get_page_height
+      pdf.set_y(h - 15)
+    end
+
+    pdf.write_html_cell(0, 5, 10, '', data[:html], data[:border], 1)
 
     pno = pdf.get_page
-    assert_equal 1, pno
+    assert_equal data[:pno], pno
 
     y = pdf.get_y
-    assert_in_delta 17.3, y, 0.1
+    assert_in_delta(data[:y], y, 0.1)
 
     no = pdf.get_num_pages
-    assert_equal 1, no
-  end
-
-  test "write_html_cell Page Break test 1" do
-    pdf = RBPDF.new
-    pdf.add_page()
-
-    pdf.set_top_margin(30)
-
-    h = pdf.get_page_height
-    pdf.set_y(h - 15)
-
-    htmlcontent = '<p>foo</p>'
-    pdf.write_html_cell(0, 5, 10, '', htmlcontent, 0, 1)
-
-    pno = pdf.get_page
-    assert_equal 2, pno
-
-    y = pdf.get_y
-    assert_in_delta 40.0, y, 0.1
-
-    no = pdf.get_num_pages
-    assert_equal 2, no
-  end
-
-  test "write_html_cell Page Break test 2" do
-    pdf = RBPDF.new
-    pdf.add_page()
-
-    pdf.set_top_margin(30)
-
-    h = pdf.get_page_height
-    pdf.set_y(h - 15)
-
-    htmlcontent = '<p>foo</p>'
-    pdf.write_html_cell(0, 5, 10, '', htmlcontent, "LRBT", 1)
-
-    pno = pdf.get_page
-    assert_equal 2, pno
-
-    y = pdf.get_y
-    assert_in_delta 40.0, y, 0.1
-
-    no = pdf.get_num_pages
-    assert_equal 2, no
+    assert_equal data[:no], no
   end
 end
