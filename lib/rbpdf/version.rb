8a1d4c66523812638b3f4ced4d23b24a4e01f744
NAITOH Jun <naitoh@gmail.com>
2018-10-06 07:42:39 +0900

    Update version 1.19.6

--- lib/rbpdf/version.rb
+++ lib/rbpdf/version.rb
@@ -1,7 +1,7 @@
-# Copyright (c) 2011-2017 NAITOH Jun
+# Copyright (c) 2011-2018 NAITOH Jun
 # Released under the MIT license
 # http://www.opensource.org/licenses/MIT
 
 module Rbpdf
-  VERSION = "1.19.5"
+  VERSION = "1.19.6"
 end
